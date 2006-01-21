4592cc7b85634cfd30e919088974bf052110599c
John W. Long <me@johnwlong.com>
2006-01-21 02:22:08 +0000

    A few refactorings. Still a bit to clean up.

--- radius/lib/radius.rb
+++ radius/lib/radius.rb
@@ -1,7 +1,21 @@
 module Radius
-	class Context; end
+  class ParseError < StandardError; end
+  
+  class MissingEndTagError < ParseError
+    def initialize(tag_name)
+      super("end tag not found for start tag `#{tag_name}'")
+    end
+  end
+  
+	class Context
+ 	  attr_accessor :prefix
+ 	  
+	  def initialize
+	    @prefix = 'radius'
+    end
+	end
 	
-	class Data
+	class Tag
 		def initialize(&b)
 			@block = b
 		end
@@ -13,32 +27,31 @@ module Radius
 		end
 	end
 
-	class DoubleTag < Data
+	class ContainerTag < Tag
 		attr_accessor :name, :attributes, :contents
+		
 		def initialize(name="", attributes={}, contents = [], &b)
-			super(&b)
 			@name, @attributes, @contents = name, attributes, contents
+			super(&b)
 		end
 	end
 		
 	class Parser
-		def initialize(options = {})
-			@options = { :pre => "pre" }.update(options)
-			@options[:context] = Context.new unless @options[:context]
-			@context = @options[:context]
+		def initialize(context = Context.new)
+			@context = context
 		end
 
 		def parse(text)
-			@stack = [DoubleTag.new { |t| t.contents.to_s }]
+			@stack = [ContainerTag.new { |t| t.contents.to_s }]
 			pre_parse(text)
 			@stack.last.to_s
 		end
 		
 		def pre_parse(text)
-			re = %r{<#{@options[:pre]}:(\w+?)(?:\s+?([^/>]*?)|)>|</#{@options[:pre]}:(\w+?)\s*?>}
+			re = %r{<#{@context.prefix}:(\w+?)(?:\s+?([^/>]*?)|)>|</#{@context.prefix}:(\w+?)\s*?>}
 			if md = re.match(text)
 				start_tag, attr, end_tag = $1, $2, $3
-				@stack.last.contents << Data.new { parse_individual(md.pre_match) }
+				@stack.last.contents << Tag.new { parse_individual(md.pre_match) }
 				remaining = md.post_match
 				if start_tag
 					parse_start_tag(start_tag, attr, remaining)
@@ -47,15 +60,15 @@ module Radius
 				end
 			else
 				if @stack.length == 1
-					@stack.last.contents << Data.new { parse_individual(text) }
+					@stack.last.contents << Tag.new { parse_individual(text) }
 				else
-					raise "parse error"
+					raise MissingEndTagError.new(@stack.last.name)
 				end
 			end
 		end
 		
 		def parse_start_tag(start_tag, attr, remaining)
-			@stack.push(DoubleTag.new(start_tag, parse_attributes(attr)))
+			@stack.push(ContainerTag.new(start_tag, parse_attributes(attr)))
 			pre_parse(remaining)
 		end
 
@@ -67,12 +80,12 @@ module Radius
 				tag.contents << popped
 				pre_parse(remaining)
 			else
-				raise "parse error: start tag not found for #{end_tag}"
+				raise MissingEndTagError.new(popped.name)
 			end
 		end
 		
 		def parse_individual(text)
-			re = /<#{@options[:pre]}:(\w+?)\s+?(.*?)\s*?\/>/
+			re = /<#{@context.prefix}:(\w+?)\s+?(.*?)\s*?\/>/
 			if md = re.match(text)
 				attr = parse_attributes($2)
 				replace = @context.send($1, attr)
--- radius/test/radius_test.rb
+++ radius/test/radius_test.rb
@@ -1,110 +1,122 @@
 require 'test/unit'
 require 'radius'
 
+class ContextTest < Test::Unit::TestCase
+  def test_initialize
+    c = Radius::Context.new
+    assert_equal 'radius', c.prefix
+  end
+end
+
 class RadiusTest < Test::Unit::TestCase
-	class TestContext < Radius::Context
-		def initialize
-			@items = ["Larry", "Moe", "Curly"]
-		end
-		
-		def echo(attr)
-			attr["text"]
-		end
-		
-		def add(attr)
-			(attr["param1"].to_i + attr["param2"].to_i).to_s
-		end
-		
-		def reverse(attr)
-			yield.reverse
-		end
-		
-		def capitalize(attr)
-			yield.upcase
-		end
-		
-		def count(attr)
-			case
-			when attr["set"]
-				@count = attr["set"].to_i
-				""
-			when attr["inc"] == "true"
-				@count = (@count || 0) + 1
-				""
-			else
-				@count.to_s				
-			end
-		end
-		
-		def loop(attr)
-			t = attr["times"].to_i
-			result = ""
-			t.times { result += yield }
-			result
-		end
-		
-		def each_item(attr)
-			result = []
-			@items.each { |@item| result << yield }
-			@item = nil
-			result.join(attr["between"] || "")
-		end
-		
-		def item(attr)
-			@item
-		end
-	end
-	def setup
-		@t = Radius::Parser.new( :pre => "wow", :context => TestContext.new )
-	end
-	def test_parse_individual
-		r = @t.parse_individual(%{<<wow:echo text="hello world!" />>})
-		assert_equal("<hello world!>", r)
+  class TestContext < Radius::Context
+    def initialize
+      @prefix = "test"
+      @items = ["Larry", "Moe", "Curly"]
+    end
+    
+    def echo(attr)
+      attr["text"]
+    end
+    
+    def add(attr)
+      (attr["param1"].to_i + attr["param2"].to_i).to_s
+    end
+    
+    def reverse(attr)
+      yield.reverse
+    end
+    
+    def capitalize(attr)
+      yield.upcase
+    end
+    
+    def count(attr)
+      case
+      when attr["set"]
+        @count = attr["set"].to_i
+        ""
+      when attr["inc"] == "true"
+        @count = (@count || 0) + 1
+        ""
+      else
+        @count.to_s       
+      end
+    end
+    
+    def loop(attr)
+      t = attr["times"].to_i
+      result = ""
+      t.times { result += yield }
+      result
+    end
+    
+    def each_item(attr)
+      result = []
+      @items.each { |@item| result << yield }
+      @item = nil
+      result.join(attr["between"] || "")
+    end
+    
+    def item(attr)
+      @item
+    end
+  end
+  
+  def setup
+    @t = Radius::Parser.new(TestContext.new )
+  end
+  
+  def test_parse_individual
+    r = @t.parse_individual(%{<<test:echo text="hello world!" />>})
+    assert_equal("<hello world!>", r)
 
-		r = @t.parse_individual(%{<wow:add param1="1" param2='2'/>})
-		assert_equal("3", r)
+    r = @t.parse_individual(%{<test:add param1="1" param2='2'/>})
+    assert_equal("3", r)
 
-		r = @t.parse_individual(%{a <wow:echo text="3 + 1 =" /> <wow:add param1="3" param2="1"/> b})
-		assert_equal("a 3 + 1 = 4 b", r)
-	end
-	def test_parse_attributes
-		r = @t.parse_attributes( %{ a="1" b='2'c="3"d="'" } )
-		assert_equal( {"a" => "1", "b" => "2", "c" => "3", "d" => "'"}, r)
-	end
-	def test_parse
-		r = @t.parse(%{<<wow:echo text="hello world!" />>})
-		assert_equal("<hello world!>", r)
+    r = @t.parse_individual(%{a <test:echo text="3 + 1 =" /> <test:add param1="3" param2="1"/> b})
+    assert_equal("a 3 + 1 = 4 b", r)
+  end
+  
+  def test_parse_attributes
+    r = @t.parse_attributes(%{ a="1" b='2'c="3"d="'" })
+    assert_equal({"a" => "1", "b" => "2", "c" => "3", "d" => "'"}, r)
+  end
+  
+  def test_parse
+    r = @t.parse(%{<<test:echo text="hello world!" />>})
+    assert_equal("<hello world!>", r)
 
-		r = @t.parse("<wow:reverse>test</wow:reverse>")
-		assert_equal("test".reverse, r)
+    r = @t.parse("<test:reverse>test</test:reverse>")
+    assert_equal("test".reverse, r)
 
-		r = @t.parse("<wow:reverse>test</wow:reverse> <wow:capitalize>test</wow:capitalize>")
-		assert_equal("test".reverse + " TEST", r)
-		
-		r = @t.parse("<wow:echo text='hello world!' /> cool: <wow:reverse>a <wow:capitalize>test</wow:capitalize> b</wow:reverse> !")
-		assert_equal("hello world! cool: b " + "TEST".reverse + " a !", r)
-		
-		r = @t.parse("<wow:reverse><wow:echo text='hello world!' /></wow:reverse>")
-		assert_equal("hello world!".reverse, r)
+    r = @t.parse("<test:reverse>test</test:reverse> <test:capitalize>test</test:capitalize>")
+    assert_equal("tset TEST", r)
+    
+    r = @t.parse("<test:echo text='hello world!' /> cool: <test:reverse>a <test:capitalize>test</test:capitalize> b</test:reverse> !")
+    assert_equal("hello world! cool: b TSET a !", r)
+    
+    r = @t.parse("<test:reverse><test:echo text='hello world!' /></test:reverse>")
+    assert_equal("!dlrow olleh", r)
 
-		r = @t.parse("<wow:reverse><wow:capitalize>test</wow:capitalize> <wow:echo text='hello world!' /></wow:reverse>")
-		assert_equal("TEST hello world!".reverse, r)
-	end
-	
-	def test_parse__2
-		r = @t.parse("<wow:reverse>12<wow:capitalize>at</wow:capitalize>34</wow:reverse>")
-		assert_equal("12AT34".reverse, r)
-	end
-	
-	def test_parse__loop
-		r = @t.parse( %{<wow:count set="0" /><wow:loop times="5"><wow:count inc="true" /><wow:count /></wow:loop>} )
-		assert_equal("12345", r)
+    r = @t.parse("<test:reverse><test:capitalize>test</test:capitalize> <test:echo text='hello world!' /></test:reverse>")
+    assert_equal("!dlrow olleh TSET", r)
 
-		r = @t.parse( %{Three Stooges: <wow:each_item between=", ">"<wow:item />"</wow:each_item>} )
-		assert_equal( %{Three Stooges: "Larry", "Moe", "Curly"}, r)
-	end
-	
-	def test_parse__fail
-		assert_raises(RuntimeError) { @t.parse("<wow:reverse>") }
-	end
+    r = @t.parse("<test:reverse>12<test:capitalize>at</test:capitalize>34</test:reverse>")
+    assert_equal("43TA21", r)
+  end
+  def test_parse__loop
+    r = @t.parse(%{<test:count set="0" /><test:loop times="5"><test:count inc="true" /><test:count /></test:loop>})
+    assert_equal("12345", r)
+
+    r = @t.parse(%{Three Stooges: <test:each_item between=", ">"<test:item />"</test:each_item>})
+    assert_equal(%{Three Stooges: "Larry", "Moe", "Curly"}, r)
+  end
+  
+  def test_parse__fail_on_no_end_tag
+    assert_raises(Radius::MissingEndTagError) { @t.parse("<test:reverse>") }
+  end
+  def test_parse__fail_on_no_end_tag_2
+    assert_raises(Radius::MissingEndTagError) { @t.parse("<test:reverse><test:capitalize></test:reverse>") }
+  end
 end
