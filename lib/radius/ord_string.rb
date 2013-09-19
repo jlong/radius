b0108bf824438c3a8b1a646460da4300825e9432
Jim Gay <jim@saturnflyer.com>
2013-09-19 11:00:22 -0400

    use ruby behavior to test for feature instead of RUBY_VERSION
    Previous code checked part of the RUBY_VERSION constant and no longer worked properly in 2.x
    This change will check the behavior of strings before applying the change, which protects against
    numbering changes in versions.

--- lib/radius/ord_string.rb
+++ lib/radius/ord_string.rb
@@ -1,6 +1,6 @@
 module Radius
   class OrdString < String
-    if RUBY_VERSION[0,3] == '1.9'
+    unless "a"[0] == 97 # test for lower than Ruby 1.9
       def [](*args)
         if args.size == 1 && args.first.is_a?(Integer)
           slice(args.first).ord
