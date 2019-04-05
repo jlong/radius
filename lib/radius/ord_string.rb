3371a50e5d9cc0264d989dc392c4cc5e5b2bebd3
Jim Gay <jim@saturnflyer.com>
2019-04-05 18:08:09 -0400

    Drop old versions of Ruby

--- lib/radius/ord_string.rb
+++ lib/radius/ord_string.rb
@@ -1,12 +1,10 @@
 module Radius
   class OrdString < String
-    unless "a"[0] == 97 # test for lower than Ruby 1.9
-      def [](*args)
-        if args.size == 1 && args.first.is_a?(Integer)
-          slice(args.first).ord
-        else
-          super
-        end
+    def [](*args)
+      if args.size == 1 && args.first.is_a?(Integer)
+        slice(args.first).ord
+      else
+        super
       end
     end
   end
