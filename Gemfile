e7b4eab374c6a15e105524ccd663767b8bc91e5b
Jim Gay <jim@saturnflyer.com>
2019-04-05 18:35:22 -0400

    Remove test-unit and simplify library loading

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ group :test do
   gem 'rake'
   gem 'kramdown'
   gem "simplecov"
-  gem 'test-unit', '2.5.5'
   gem 'coveralls', :require => false
   gem 'minitest'
 end
@@ -17,6 +16,5 @@ platforms :rbx do
   gem 'racc'                     # if using gems like ruby_parser or parser
   gem 'rubysl', '~> 2.0'
   gem 'psych'
-  gem 'rubysl-test-unit'         # if using test-unit with minitest 5.x https://github.com/rubysl/rubysl-test-unit/issues/1
   gem 'rubinius-developer_tools'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+require 'timeout'
 require 'simplecov'
 SimpleCov.start do
   add_filter 'test'
@@ -8,39 +9,32 @@ if ENV['COVERALLS']
   Coveralls.wear!
 end
 
-require 'timeout'
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 
-unless defined? RADIUS_LIB
+require 'radius'
 
-  RADIUS_LIB = File.join(File.dirname(__FILE__), '..', 'lib')
-  $LOAD_PATH << RADIUS_LIB
+module RadiusTestHelper
+  class TestContext < Radius::Context; end
 
-  require 'radius'
-  require 'minitest'
-
-  module RadiusTestHelper
-    class TestContext < Radius::Context; end
-
-    def new_context
-      Radius::Context.new do |c|
-        c.define_tag("reverse"   ) { |tag| tag.expand.reverse }
-        c.define_tag("capitalize") { |tag| tag.expand.upcase  }
-        c.define_tag("echo"      ) { |tag| tag.attr['value']  }
-        c.define_tag("wrap"      ) { |tag| "[#{tag.expand}]"  }
-        c.define_tag("attr") do |tag|
-          kv = tag.attr.keys.sort.collect{|k| "#{k.inspect}=>#{tag[k].inspect}"}
-          "{#{kv.join(', ')}}"
-        end
+  def new_context
+    Radius::Context.new do |c|
+      c.define_tag("reverse"   ) { |tag| tag.expand.reverse }
+      c.define_tag("capitalize") { |tag| tag.expand.upcase  }
+      c.define_tag("echo"      ) { |tag| tag.attr['value']  }
+      c.define_tag("wrap"      ) { |tag| "[#{tag.expand}]"  }
+      c.define_tag("attr") do |tag|
+        kv = tag.attr.keys.sort.collect{|k| "#{k.inspect}=>#{tag[k].inspect}"}
+        "{#{kv.join(', ')}}"
       end
     end
+  end
 
-    def define_tag(name, options = {}, &block)
-      @parser.context.define_tag name, options, &block
-    end
+  def define_tag(name, options = {}, &block)
+    @parser.context.define_tag name, options, &block
+  end
 
-    def define_global_tag(name, options = {}, &block)
-      @context.define_tag name, options, &block
-    end
+  def define_global_tag(name, options = {}, &block)
+    @context.define_tag name, options, &block
   end
 end
 require 'minitest/autorun'
